6663240bab7e6d9b71ff41f1df947d4d668602b5
AHMED EL BASAL <AHMED EL BASAL@DESKTOP-8GB63NT>
2021-03-06 00:05:48 +0200

    update proj todo

--- db-connection.js
+++ db-connection.js
@@ -1,8 +1,9 @@
 // get mongoose module
 const mongoose = require('mongoose')
 
-
-mongoose.connect(process.env.MONGODB_URL, {
+// connect to database     // LOCAL DATABASE mongodb://localhost:27017/todoApp
+//  mongodb+srv://AhmedBassal:ZlZN50Nc0wav0uqL@cluster0.2oocx.mongodb.net/todoApp?retryWrites=true&w=majority
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/todoApp', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: true,
