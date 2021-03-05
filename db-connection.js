732f3e7d42af0f09ad559d244ad5c2e9d4134f1f
AHMED EL BASAL <AHMED EL BASAL@DESKTOP-8GB63NT>
2021-03-05 23:35:06 +0200

    upload backend for todoList

--- db-connection.js
+++ db-connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 
 // connect to database     // LOCAL DATABASE mongodb://localhost:27017/todoApp
 //  mongodb+srv://AhmedBassal:ZlZN50Nc0wav0uqL@cluster0.2oocx.mongodb.net/todoApp?retryWrites=true&w=majority
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://AhmedBassal:ZlZN50Nc0wav0uqL@cluster0.2oocx.mongodb.net/todoApp?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: true,
